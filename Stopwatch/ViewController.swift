2b6c85d56b30affb8f624314835c635c8acc8b16
nicholasbair <nicholasbair@Nicholass-MBP.home>
2015-06-21 16:18:47 -0400

    Stopwatch App
    Created in Complete iOS8 and Swift Course on Udemy

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    
+    var count = 0
+    
+    func updateTime() {
+        
+        count++
+        
+        lblCounter.text = "\(count)"
+        
+    }
 
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBOutlet weak var lblCounter: UILabel!
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        count = 0
+        
+        lblCounter.text = "0"
+        
+    }
+    
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
